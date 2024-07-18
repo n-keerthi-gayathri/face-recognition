73663621392ff0e1ec352f4058d8a4a4b7ddb124
Priyanka <priyankaippala2003@gmail.com>
2024-07-18 09:47:44 +0530

    changes page.tsx and next.config.mjs files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+  ...nextConfig,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/:path*', // Ensure this points to your Flask server
+      },
+    ];
+  },
+};
